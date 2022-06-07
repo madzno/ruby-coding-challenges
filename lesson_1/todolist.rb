35a14cbf8720acda919cea1399dab6453335fdad
Madz No <madzno12@gmail.com>
2022-06-07 11:45:09 -0400

    updating todolist assignment

---
+++ lesson_1/todolist.rb
@@ -0,0 +1,152 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+    description == otherTodo.description &&
+    done == otherTodo.done
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    if todo.class == Todo
+      @todos << todo
+    else
+      raise TypeError, "Can only add Todo objects"
+    end
+  end
+  alias_method :<<, :add
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def to_a
+    @todos.clone
+  end
+
+  def done?
+    @todos.all? do |item|
+      item.done == true
+    end
+  end
+
+  def item_at(idx)
+    @todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    @todos.fetch(idx).done = true
+  end
+
+  def mark_undone_at(idx)
+    @todos.fetch(idx).done = false
+  end
+
+  def done!
+    @todos.each do |item|
+      item.done = true
+    end
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(idx)
+    deleted_item = @todos.fetch(idx)
+    @todos.delete(deleted_item)
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def each
+    idx = 0
+    loop do
+      yield(@todos[idx])
+      idx += 1
+      break if idx >= @todos.size
+    end
+    self
+  end
+
+  def select
+    results = TodoList.new('Selected todos')
+    @todos.each do |item|
+      results.todos << item if yield(item)
+    end
+    results
+  end
+
+  def find_by_title(string)
+
+  end
+
+  protected
+
+  attr_accessor :todos
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+results = list.select { |todo| todo.done? }    # you need to implement this method
+
+puts results.inspect
